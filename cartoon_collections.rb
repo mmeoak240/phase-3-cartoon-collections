526c97915d9e92ed48d77e85588b0b21522872a2
Michael Meoak <mmeoak240@gmail.com>
2022-06-13 14:54:40 -0700

    commit tree 857cfd93830b3aa9be65054f260f8b25549f2955

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves dwarves
+  dwarves.each.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet elements
+  summon = []
+  elements.map do |element|
+    summon << "#{element.capitalize}" + "!"
+  end
+  summon
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls calls
+  truthy_arr = calls.map do |call|
+    call.length > 4 ?
+    true : false
+  end
+  results = truthy_arr.include? true
+  results
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+
+
+def find_the_cheese ingredients
+  cheeses = ["cheddar", "gouda", "camembert"]
+  ingredients.find do |ingredient|
+    cheeses.include? (ingredient)
+  end
 end
